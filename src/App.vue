e372b064fc1d589fdb9a0e4c480c216bec69a66b
Anselm <anselm@users-MacBook-Pro.local>
2023-12-06 19:05:08 +0100

    implement counter

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import {ref} from 'vue'
+  let count = ref(0)
+  const incrementCounter = () => count.value++
+  const decrementCounter = () => count.value--
+</script>
+
+<template>
+  <main>
+    <h1>Vue Counter</h1>
+    <h3 >{{count}}</h3>
+    <div><button class="actions" @click="decrementCounter"> - </button><button class="actions" @click="incrementCounter"> + </button></div>
+  </main>
+</template>
+
+<style scoped>
+  span {
+    border: solid, 5px;
+    border-radius: 2px;
+    padding: 20px;
+  }
+</style>
